e2cda0fb8d998fdfd428af0d5b753d35d1ccf1e2
abubakkariiui <abubakkarmit@gmail.com>
2024-04-23 23:21:45 +0500

    udpate opacity of overlay

--- src/components/Search/Overlay.tsx
+++ src/components/Search/Overlay.tsx
@@ -24,7 +24,7 @@ const Overlay = ({
     <>
       {showOverlay && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+          className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center"
           onClick={onClose}
         >
           <div
